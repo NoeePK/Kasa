aa8c1f9be9b09729c4db0651e825a645128e25de
Noee Pisano <noeepisanokoffel@gmail.com>
2022-11-29 11:58:34 +0100

    squelette du site

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from '../logo.svg';
+import '../App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>KASA</h1>
+        <h1>Courage !</h1>
+        <h2>Tu peux le faire !</h2>
         <a
           className="App-link"
           href="https://reactjs.org"
