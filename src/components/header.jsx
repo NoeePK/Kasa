0177f9e044167910900b04b0c6f0014bf6bf4109
Noee Pisano <noeepisanokoffel@gmail.com>
2022-11-30 10:05:00 +0100

    warning : utiliser majuscules

---
+++ src/components/header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+    return (<div>
+        <Header className="header">
+            <Figure className="headerLogo">
+                <img className>
+                </img>
+
+            </Figure>
+
+        </Header>
+    </div>)
+}
