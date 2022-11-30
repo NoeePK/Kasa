6ff760ad80be02f02d4e8ee2315b2ea45f46ac0a
Noee Pisano <noeepisanokoffel@gmail.com>
2022-11-30 10:27:25 +0100

    banner et nav : essai

---
+++ src/components/banner.jsx
@@ -0,0 +1,10 @@
+// Importer l'image de fond
+// Plutôt l'importer dans le css
+
+export function Banner() {
+    return (
+        <div className="banner-container">
+            <h1>Chez vous, partout et ailleurs</h1>
+        </div>
+    )
+}
---
+++ src/components/navigation.jsx
@@ -0,0 +1,15 @@
+// Importer les liens de la nav
+
+export function Navigation() {
+    return (
+        <Nav className="navigation-container">
+            <a className="navigation-link-home">
+                Accueil
+
+            </a>
+            <a className="navigation-link-aboutUs">
+                À propos
+            </a>
+        </Nav>
+    )
+}
