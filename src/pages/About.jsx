f0723d0f7c32783b858c635fccd7b7787abdfa50
Noee Pisano <noeepisanokoffel@gmail.com>
2022-12-09 10:51:45 +0100

    wip : utilisation map à propos

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,42 +4,23 @@
 import Banner from "../components/Banner";
 import Collapse from "../components/Collapse";
 
+// Importer les titres et textes
+import { AboutText } from "../datas/About";
+
 export function About() {
     return (
         <section className="about-container">
             <Banner />
-            <article className="about-articles">
-                <h3 className="about-titles">
-                    Fiabilité
-                </h3>
-                <p>
-                    Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes.
-                </p>
-            </article>
-            <article className="about-articles">
-                <h3 className="about-titles">
-                    Respect
-                </h3>
-                <p>
-                    La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de pertubation du voisinage entraînera une exclusion de notre plateforme.
-                </p>
-            </article>
-            <article className="about-articles">
-                <h3 className="about-titles">
-                    Service
-                </h3>
-                <p>
-                    Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question.
-                </p>
-            </article>
-            <article className="about-articles">
-                <h3 className="about-titles">
-                    Sécurité
-                </h3>
-                <p>
-                    La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.
-                </p>
-            </article>
+            {AboutText.map((id) => (
+                <article className="about-articles">
+                    {AboutText.map((titles) => (
+                        <h3 className="about-titles">{titles.title}</h3>
+                    ))}
+                    {AboutText.map((texts) => (
+                        <p>{texts.text}</p>
+                    ))}
+                </article>
+            ))}
         </section>
     )
 };
