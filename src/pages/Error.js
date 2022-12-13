464983ddb9500c05e031b8f1169605fd837db0e9
Noee Pisano <noeepisanokoffel@gmail.com>
2022-12-13 14:32:25 +0100

    error figcaption

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -5,10 +5,12 @@ import image from "../assets/logo-error.png";
 export function Error() {
     return (
         <section className="error-container">
-            <img src={image} alt="404" className="error-logo" />
-            <h1 className="error-text">
-                Oups ! La page que vous demandez n'existe pas.
-            </h1>
+            <figure>
+                <img src={image} alt="404" className="error-logo" />
+                <figcaption className="error-text">
+                    Oups ! La page que vous demandez n'existe pas.
+                </figcaption>
+            </figure>
             <Link to="/" className="to-home-link">
                 Retourner sur la page d'accueil
             </Link>
