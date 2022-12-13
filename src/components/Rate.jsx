96cfa03a138082cebf55f32ed26f02b305770e8c
Noee Pisano <noeepisanokoffel@gmail.com>
2022-12-13 14:44:22 +0100

    suppr fichiers hors kasa

---
+++ src/components/Rate.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import iconFull from "../assets/full-star.svg";
+import iconEmpty from "../assets/empty-star.svg";
+// Utiliser la value dans Rental.js
+// Utiliser une condition ?
+
+export function Rate(value) {
+    return <div className="rate"></div>;
+}
+
+export default Rate;
---
+++ src/components/collapse.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import arrow_down from "../assets/arrow_down.svg";
+import arrow_up from "../assets/arrow_up.svg";
+
+// Utiliser les props ?
+
+export function Collapse() {
+    return <div></div>;
+}
+
+export default Collapse;
