e51cb404fa25e99b3aa39a3b7fbd3f9290d47e14
Noee Pisano <noeepisanokoffel@gmail.com>
2022-12-13 14:26:15 +0100

    structure composant Tags

---
+++ src/components/Tags.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+// Utiliser "tags=" dans Rental.js
+// Passer "tags" en paramètre qui sera utilisé dans Rental pour afficher les bons tags
+
+export function Tags(tags) {
+    return (
+        <ul className="tags">
+            {tags.map((index, tag) => (
+                <li key={index}>{tag}</li>
+            ))}
+        </ul>
+    );
+}
+
+export default Tags;
