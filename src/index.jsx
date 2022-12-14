d47d05f1ec24b1c596c189ea926d60171e87db10
Noee Pisano <noeepisanokoffel@gmail.com>
2022-12-14 12:01:16 +0100

    mise en place :root font size

--- src/index.jsx
+++ src/index.jsx
@@ -3,8 +3,8 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router} from "react-router-dom";
 import App from "./App";
 
-// Style applicable à toute l'application
-import "/";
+// Style applicable à TOUTE l'application
+import "./styles/compiled/index.css";
 
 const root_container = document.getElementById("root");
 const root = createRoot(root_container);
