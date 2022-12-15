b9d31310b1931d7b1e50fdc810ff60a7bdcfbae2
Noee Pisano <noeepisanokoffel@gmail.com>
2022-12-15 13:44:55 +0100

    sauvegarde

--- src/components/carrousel.jsx
+++ src/components/carrousel.jsx
@@ -2,34 +2,42 @@ import React, { useState } from "react";
 
 import left from "../assets/arrow-left.svg";
 import right from "../assets/arrow-right.svg";
-import Rental from "../datas/Rentals";
 
 function Carrousel(props) {
 
-// Récupérer le nombre de photo
+const pictures = props.pictures;
+// Récupérer la longueur de l'array
 const length = props.pictures.length;
-// Mettre en place un state pour que l'image présente soit la num 0
+// Définir l'index de la première slide
 const [ current, setCurrent ] = useState(0);
 
-
 // Avant premier => dernier
+// SI la photo est la première,
+// SOIT c'est le cas, donc on passe sur la dernière
+// SOIT c'est pas le cas, donc on passe à la précédente
     const previousSlide = () => {
-        setCurrent(current === 0)
+        setCurrent(current === 0 ? length - 1 : current -1)
     };
     // Après dernier => premier
     const nextSlide = () => {
-        setCurrent(current === 0);
+        setCurrent(current === -1 ? 0 : current + 1);
     };
 
     return (
         <section className="carrousel-container">
             <span className="arrow-left" onClick={previousSlide}>
-                <img src={left} alt="previous picture"></img>
+                <img src={left} alt="previous"></img>
             </span>
             <span className="arrow-right" onClick={nextSlide}>
-                <img src={right} alt="next picture"></img>
+                <img src={right} alt="next"></img>
             </span>
-        </section>
+
+            {pictures.map((index, picture) => (
+                <div key={index}>
+                    {index === current &&
+                    <img src={picture} alt=""></img>}
+                </div>
+            ))}        </section>
     );
 }
 
