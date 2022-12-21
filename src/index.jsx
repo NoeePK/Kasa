6a4cd93397498744671456cdf699fdbc2dfd40eb
Noee Pisano <noeepisanokoffel@gmail.com>
2022-12-21 11:55:51 +0100

    sauvegarde

--- src/index.jsx
+++ src/index.jsx
@@ -5,8 +5,8 @@ import App from "./App";
 
 // Style applicable à TOUTE l'application
 import "./styles/compiled/index.css";
-import "./styles/compiled/small.css";
-import "./styles/compiled/xsmall.css";
+import "./styles/compiled/tablet.css";
+import "./styles/compiled/mobile.css";
 
 const root_container = document.getElementById("root");
 const root = createRoot(root_container);
